7ce5d30786647fa569e6a5d096f5777c964233ea
TAbdiukov <TAbdiukov@gmail.com>
2024-07-01 01:46:38 +0200

    Helpy - even better structured

--- helpy.py
+++ helpy.py
@@ -18,6 +18,7 @@ class Helpy:
 
 		self.si = None
 		self.paste = None
+		self.payload = None
 
 	@staticmethod
 	def clear_buffer():
@@ -26,20 +27,9 @@ class Helpy:
 	def gen_si(self, s):
 		self.si = int(s, 16)
 
-	def is_input_valid(self, s):
-		if(s[:1].isdigit() and (len(s) > 6) and (int(s, 16) > 0)):
-			self.gen_si(s)
-			test = self.si & 0xFFFFFFFFF0000000
-			if(test > 0):
-				print("(Warning) Invalid PSP address")
-				Helpy.clear_buffer()
-				return False
-			return True
-		return False
-
 	def toaddr32(self, num): return '0x{0:08X}'.format(num)
 
-	def gen_pay(self):
+	def gen_std_pay(self):
 		assert self.si > 0
 
 		tmp = self.si
@@ -51,7 +41,24 @@ class Helpy:
 		arg1 = self.toaddr32(self.si)
 
 		payload = f"_L {arg1} 0x01234567 // Helpy-automated: set address to 0x01234567"
-		return payload
+		self.payload = payload
+
+	def output_pay(self):
+		pay = self.payload
+		print("▒Output: \n"+pay)
+		pyperclip.copy(pay)
+
+	def is_input_valid(self, s):
+		if(s[:1].isdigit() and (len(s) > 6) and (int(s, 16) > 0)):
+			self.gen_si(s)
+			test = self.si & 0xFFFFFFFFF0000000
+			if(test > 0):
+				self.payload = "*Invalid PSP address: "+s
+				self.output_pay()
+				
+				return False
+			return True
+		return False
 
 	def main(self):
 		HOW_TO = """Just copy standard address to clipboard!"""
@@ -65,14 +72,14 @@ class Helpy:
 
 				# check for valid input
 				if(self.is_input_valid(self.paste)):
-					print("*Valid input: "+self.paste)
+					print("*Valid PSP address: "+self.paste)
 					# generate payload
-					pay = self.gen_pay()
-					print("*Output: \n"+pay)
-					pyperclip.copy(pay)
+					self.gen_std_pay()
+					# print payload
+					self.output_pay()
 				time.sleep(0.1)
 		except KeyboardInterrupt:
-			print('Exiting...')
+			print('\nExiting...')
 
 if __name__ == '__main__':
 	helpy = Helpy()
