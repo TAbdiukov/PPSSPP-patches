aa53f54da112ab9c1707e097be5cdd87179e7dcf
TAbdiukov <TAbdiukov@gmail.com>
2024-07-01 01:36:01 +0200

    Better structured code

--- helpy.py
+++ helpy.py
@@ -15,28 +15,39 @@ class Helpy:
 		self.MAGIC = 0x08800000
 		self.OP_EDIT_4BYTES = 0x20000000
 		self.VAL = 1
+
 		self.si = None
 		self.paste = None
 
+	@staticmethod
+	def clear_buffer():
+		pyperclip.copy(' ')
+
+	def gen_si(self, s):
+		self.si = int(s, 16)
+
 	def is_input_valid(self, s):
 		if(s[:1].isdigit() and (len(s) > 6) and (int(s, 16) > 0)):
+			self.gen_si(s)
+			test = self.si & 0xFFFFFFFFF0000000
+			if(test > 0):
+				print("(Warning) Invalid PSP address")
+				Helpy.clear_buffer()
+				return False
 			return True
 		return False
 
 	def toaddr32(self, num): return '0x{0:08X}'.format(num)
 
-	def gen_pay(self, s):
-		self.si = int(s, 16)
+	def gen_pay(self):
 		assert self.si > 0
 
-		self.si = self.si - self.MAGIC
+		tmp = self.si
+		tmp = tmp - self.MAGIC
 
-		test = self.si & 0xFFFFFFFFF0000000
-		if(test > 0):
-			print("(Warning) Invalid PSP address")
+		tmp = tmp & 0x0FFFFFFF
+		tmp = tmp + self.OP_EDIT_4BYTES
 
-		self.si = self.si & 0x0FFFFFFF
-		self.si = self.si + self.OP_EDIT_4BYTES
 		arg1 = self.toaddr32(self.si)
 
 		payload = f"_L {arg1} 0x01234567 // Helpy-automated: set address to 0x01234567"
@@ -54,9 +65,9 @@ class Helpy:
 
 				# check for valid input
 				if(self.is_input_valid(self.paste)):
-					print("*Valid in: "+self.paste)
-					# gen payload
-					pay = self.gen_pay(self.paste)
+					print("*Valid input: "+self.paste)
+					# generate payload
+					pay = self.gen_pay()
 					print("*Output: \n"+pay)
 					pyperclip.copy(pay)
 				time.sleep(0.1)
